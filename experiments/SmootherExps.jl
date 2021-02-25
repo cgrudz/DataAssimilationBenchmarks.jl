d60fc1306de3e5d2b07c8cc7239e38b95e10620f
Colin J Grudzien <cgrudzien@caprine.math.unr.edu>
2021-02-24 20:51:37 -0800

    preping for caprine

--- experiments/SmootherExps.jl
+++ experiments/SmootherExps.jl
@@ -819,7 +819,7 @@ function iterative_state(args::Tuple{String,String,Int64,Int64,Int64, Bool, Floa
     f_steps = convert(Int64, tanl / h)
 
     # number of analyses
-    nanl = 250
+    nanl = 25000
 
     # set seed 
     Random.seed!(seed)
