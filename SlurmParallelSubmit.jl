8062f618e01eb5496da008db139c8362c2d1177c
Colin J Grudzien <cgrudzien@caprine.math.unr.edu>
2021-02-24 20:57:46 -0800

    preping for MDA experiment

--- SlurmParallelSubmit.jl
+++ SlurmParallelSubmit.jl
@@ -17,8 +17,8 @@ using FilterExps, SmootherExps, EnsembleKalmanSchemes, DeSolvers, L96, JLD, Debu
 # timeseries are named by the model, seed to initialize, the integration scheme used to produce, number of analyses,
 # the spinup length, and the time length between observation points
 #
-#time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.00_tanl_0.05_nanl_50000_spin_5000_h_0.010.jld"
-time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.00_tanl_0.10_nanl_50000_spin_5000_h_0.010.jld"
+time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.00_tanl_0.05_nanl_50000_spin_5000_h_0.010.jld"
+#time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.00_tanl_0.10_nanl_50000_spin_5000_h_0.010.jld"
 #time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.10_tanl_0.05_nanl_50000_spin_5000_h_0.005.jld"
 #time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.10_tanl_0.10_nanl_50000_spin_5000_h_0.005.jld"
 ########################################################################################################################
@@ -64,10 +64,43 @@ time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.00_tanl_
 # hybrid_state single run for degbugging, arguments are
 # [time_series, method, seed, lag, shift, mda, obs_un, obs_dim, N_ens, state_infl = args
 #
-#schemes = ["etks"]
+schemes = ["etks"]
+seed = 0
+lag = 1:3:52
+shift = 1
+obs_un = 1.0
+obs_dim = 40
+N_ens = 15:2:43
+state_infl = LinRange(1.00, 1.10, 11)
+mda = true
+
+# load the experiments
+args = Tuple[]
+for scheme in schemes
+    for l in lag
+        for N in N_ens
+            for s_infl in state_infl
+                tmp = (time_series, scheme, seed, l, shift, mda, obs_un, obs_dim, N, s_infl)
+                push!(args, tmp)
+            end
+        end
+    end
+end
+
+name = "/home/cgrudzien/da_benchmark/data/input_data/hybrid_state_smoother_input_args.jld"
+save(name, "experiments", args)
+
+########################################################################################################################
+########################################################################################################################
+# Iterative smoothers
+########################################################################################################################
+# submit the experiments given the parameters and write to text files over the initializations
+# [time_series, method, seed, lag, shift, mda, obs_un, obs_dim, N_ens, infl] = args
+#
+#schemes = ["ienks-bundle"]
 #seed = 0
 #lag = 1:3:52
-#shift = 2
+#shift = 1
 #obs_un = 1.0
 #obs_dim = 40
 #N_ens = 15:2:43
@@ -87,42 +120,9 @@ time_series = "./data/timeseries/l96_timeseries_seed_0000_dim_40_diff_0.00_tanl_
 #    end
 #end
 #
-#name = "/home/cgrudzien/da_benchmark/data/input_data/hybrid_state_smoother_input_args.jld"
+#name = "/home/cgrudzien/da_benchmark/data/input_data/iterative_state_smoother_input_args.jld"
 #save(name, "experiments", args)
 #
-########################################################################################################################
-########################################################################################################################
-# Iterative smoothers
-########################################################################################################################
-# submit the experiments given the parameters and write to text files over the initializations
-# [time_series, method, seed, lag, shift, mda, obs_un, obs_dim, N_ens, infl] = args
-#
-schemes = ["ienks-bundle"]
-seed = 0
-lag = 1:3:52
-shift = 1
-obs_un = 1.0
-obs_dim = 40
-N_ens = 15:2:43
-state_infl = LinRange(1.00, 1.10, 11)
-mda = false
-
-# load the experiments
-args = Tuple[]
-for scheme in schemes
-    for l in lag
-        for N in N_ens
-            for s_infl in state_infl
-                tmp = (time_series, scheme, seed, l, shift, mda, obs_un, obs_dim, N, s_infl)
-                push!(args, tmp)
-            end
-        end
-    end
-end
-
-name = "/home/cgrudzien/da_benchmark/data/input_data/iterative_state_smoother_input_args.jld"
-save(name, "experiments", args)
-
 ########################################################################################################################
 # Loop submit the experiments in the above to the queue
 ########################################################################################################################
