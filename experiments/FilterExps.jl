3c2b1a4335132a8258280b987578fbd2370f0c97
Colin J Grudzien <cgrudzien@caprine.math.unr.edu>
2021-05-18 20:10:15 -0700

    running mlef and mlef linesearch on caprine, with finite size versions on okapi

--- experiments/FilterExps.jl
+++ experiments/FilterExps.jl
@@ -34,7 +34,7 @@ function filter_state(args::Tuple{String,String,Int64,Float64,Int64,Float64,Int6
     f_steps = convert(Int64, tanl / h)
 
     # number of analyses
-    nanl = 25
+    nanl = 25000
 
     # set seed 
     Random.seed!(seed)
