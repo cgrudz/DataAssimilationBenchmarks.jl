a04c515b9dc2f644534495d22ffd4cbbf17a054d
Colin J Grudzien <cgrudzien@caprine.math.unr.edu>
2021-05-26 23:39:12 -0700

    Fixing syntax, using copies of the rename function in the directory
    Glob won't accept the absolute path with leading / so this needs to
    be copied to scractch space to be ued from there.

--- data/Rename.jl
+++ data/Rename.jl
@@ -2,6 +2,7 @@
 module Rename 
 ########################################################################################################################
 # imports and exports
+using Debugger
 using Glob
 using JLD
 
@@ -10,19 +11,20 @@ export rename_files
 ########################################################################################################################
 
 function rename_files()
-    fnames = Glob.glob("./mles-transform_single_iteration/*")
+    fnames = Glob.glob("/x/capa/scratch/cgrudzien/final_experiment_data/versus_operator/mles-transform_single_iteration/*")
     for name in fnames
         split_name = split(name, "_")
-        tmp = [split_name[1:22]; [lpad(parse(Float64, split_name[23]), 5, "0")]; split_name[24:end]]
+        tmp = [split_name[1:25]; [lpad(parse(Float64, split_name[26]), 5, "0")]; split_name[27:end]]
         rename = ""
         string_len = (length(tmp)-1)
         for i in 1:string_len
             rename *= tmp[i] * "_"
         end
         rename *= tmp[end]
+        @bp
         print(rename)
-        my_command = `mv $name $rename`
-        run(my_command)
+        #my_command = `mv $name $rename`
+        #run(my_command)
     end
  end
  
