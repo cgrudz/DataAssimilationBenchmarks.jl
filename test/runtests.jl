d5cc76bd525d2fc15438459018d9cac1c0b9be9a
Colin Grudzien <cgrudz@mailbox.org>
2021-08-19 15:45:39 -0700

    Cleaning up test cases
    Starting to work on updates to the merged branch, want to update
    this to run all test cases together in the runtests.jl module when
    instantiating the test command.  Preparing to add additional test
    cases in the main module, will merge this later with additional
    work to the time series generation test.

---
+++ test/runtests.jl
@@ -0,0 +1,39 @@
+########################################################################################################################
+module runtests
+########################################################################################################################
+########################################################################################################################
+# imports and exports
+using DataAssimilationBenchmarks.DeSolvers
+using DataAssimilationBenchmarks.L96
+using Test
+
+########################################################################################################################
+########################################################################################################################
+# first testset using Euler Maruyama
+@testset "Euler Maruyama" begin
+    # initial conditions and arguments
+    x = zeros(40)
+    
+    # step size
+    h = 0.01
+    
+    # forcing parameter
+    f = 8.0
+    kwargs = Dict{String, Any}(
+        "h" => h,
+        "diffusion" => 0.0,
+        "dx_params" => [f],
+        "dx_dt" => L96.dx_dt,
+        )
+    
+    # parameters to test
+    # em_step! writes over x in place
+    em_step!(x, 0.0, kwargs)
+    
+    # evaluate test pass/fail if the vector of x is equal to (f*h) in every instance
+    @test sum(x .== (f*h)) == 40
+end
+
+########################################################################################################################
+
+end
