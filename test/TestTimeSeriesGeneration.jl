6630f33cc23844c635f95f97d109bb225b507a66
Colin Grudzien <cgrudz@mailbox.org>
2021-09-18 12:26:36 -0700

    Adding comments and cleaning files

--- test/TestTimeSeriesGeneration.jl
+++ test/TestTimeSeriesGeneration.jl
@@ -8,27 +8,27 @@ using JLD
 using Random
 
 #######################################################################################################################
-    # initial conditions and arguments
+# Test generation of the L96 model time series
 
-
+# define the model and the solver
 dx_dt = L96.dx_dt
 step_model! = DeSolvers.em_step!
 
+# set model and experimental parameters
 F = 8.0
-spin = 100
 h = 0.001
 nanl = 1000
 sys_dim = 40
 diffusion = 0.1
-tanl = 0.1
+tanl = 0.01
 seed = 0
-
-dx_params = Dict{String, Array{Float64}}("F" => [F])
-
-fore_steps = convert(Int64, tanl/h)
-
 Random.seed!(seed)
 
+# define the dx_params dict
+dx_params = Dict{String, Array{Float64}}("F" => [F])
+fore_steps = convert(Int64, tanl/h)
+
+# set the kwargs for the integration scheme
 kwargs = Dict{String, Any}(
           "h" => h,
           "diffusion" => diffusion,
@@ -36,21 +36,22 @@ kwargs = Dict{String, Any}(
           "dx_dt" => L96.dx_dt,
          )
 
-#xt = np.ones(sys_dim)
+# set arbitrary initial condition
 xt = ones(sys_dim)
 
-#tobs = np.zeros ([sys_dim, nanl])
+# pre-allocate storage for the time series observations
 tobs = Array{Float64}(undef,sys_dim, nanl)
-#for i in range(nanl)
+
+# loop the experiment, taking observations at time length tanl
 for i in 1:nanl
-    #for j in range(fore_steps)
     for j in 1:fore_steps
         step_model!(xt, 0.0, kwargs)
     end
     tobs[:,i] = xt
 end
 
-
+# define the file name for the experiment output
+# dynamically based on experiment parameters
 fname = "time_series_data_seed_" * lpad(seed, 4, "0") *
         "_dim_" * lpad(sys_dim, 2, "0") *
         "_diff_" * rpad(diffusion, 5, "0") *
@@ -60,7 +61,7 @@ fname = "time_series_data_seed_" * lpad(seed, 4, "0") *
         "_h_" * rpad(h, 5, "0") *
         ".jld"
 
-
+# define the experimental data in a dictionary to write with JLD
 data = Dict{String, Any}(
     "h" => h,
     "diffusion" => diffusion,
@@ -72,10 +73,27 @@ data = Dict{String, Any}(
     )
 path = "../data/time_series/"
 
-    try
-        save(path * fname, data)
-        did_write = true
-    catch
-        did_write = false
-    end
+
+# test to see if the data can be written to standard output directory
+try
+    save(path * fname, data)
+    did_write = true
+catch
+    # if not, set test case false
+    did_write = false
+end
+
+
+# test to see if the data can be read from standard output directory
+try
+    tmp = load(path * fname)
+    did_read = true
+catch
+    # if not, set test case false
+    did_read = false
+end
+
+
+#######################################################################################################################
+
 end
