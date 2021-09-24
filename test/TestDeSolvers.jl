7e950c5c97314cfa6b089b4f9c6c765ada73fab3
Sukhreen Sandhu <sukhreens@nevada.unr.edu>
2021-09-24 14:16:42 -0700

    hierarchy

--- test/TestDeSolvers.jl
+++ test/TestDeSolvers.jl
@@ -1,5 +1,5 @@
 ########################################################################################################################
-module RunTest
+module TestDeSolvers
 ########################################################################################################################
 ########################################################################################################################
 # imports and exports
@@ -7,29 +7,35 @@ using DataAssimilationBenchmarks.DeSolvers
 using DataAssimilationBenchmarks.L96
 using Test
 
+export test1
 ########################################################################################################################
 ########################################################################################################################
 # first testset using Euler Maruyama
-@testset "Euler Maruyama" begin
+#@testset "Euler Maruyama" begin
     # initial conditions and arguments
     x = zeros(40)
+
     # step size
     h = 0.01
+    F = 8.0
+
+    dx_params = Dict{String, Array{Float64}}("F" => [F])
     # forcing parameter
-    f = 8.0
+
     kwargs = Dict{String, Any}(
         "h" => h,
         "diffusion" => 0.0,
-        "dx_params" => [f],
+        "dx_params" => dx_params,
         "dx_dt" => L96.dx_dt,
         )
+
     # parameters to test
     # em_step! writes over x in place
     em_step!(x, 0.0, kwargs)
+
     # evaluate test pass/fail if the vector of x is equal to (f*h) in every instance
-    @test sum(x .== (f*h)) == 40
-        # if conditions are true, pass
-end
+    test1=sum(x .== (F*h)) == 40
+
 
 ########################################################################################################################
 
