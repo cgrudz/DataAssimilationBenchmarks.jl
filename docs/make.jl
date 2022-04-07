7ed8177b247272dfcb8b7d4623c027cf3b781d31
cgrudz <cgrudz@mailbox.org>
2022-04-06 17:48:21 -0700

    Attempting an automatic document deploy

--- docs/make.jl
+++ docs/make.jl
@@ -22,9 +22,6 @@ makedocs(
             ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/cgrudz/DataAssimilationBenchmarks.jl.git",
+)
